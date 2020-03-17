6178b3f85a2ac54292ef7df888c81d95dc041228
Akaur23-tech <60752250+Akaur23-tech@users.noreply.github.com>
2020-03-17 13:39:06 -0700

    added automated slideshow

--- scripts/show-nav.js
+++ scripts/show-nav.js
@@ -33,5 +33,21 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+// Automatic slideshow in main page
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
 
   
