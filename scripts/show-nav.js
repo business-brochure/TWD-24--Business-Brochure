8fcf51c0f26343cc14a70e822508a1cfd6dcb0c8
Akaur23-tech <60752250+Akaur23-tech@users.noreply.github.com>
2020-03-17 18:01:10 -0700

    go to top button added

--- scripts/show-nav.js
+++ scripts/show-nav.js
@@ -48,6 +48,30 @@ function carousel() {
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 3000); // Change image every 2 seconds
 }
+//When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    gotoTop.style.visibility = 'visible';
+  } else {
+    gotoTop.style.visibility ='hidden';
+  }
+}
+
+//When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+  
+ // Smooth Scroll
+ const scroll = new SmoothScroll('a[href*="#"]',  {
+      offset: 20,
+      speed:500,
+      speedAsDuration: true,
+      durationMax:500
+  });
+  
 
   
